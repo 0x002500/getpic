a4c62bc59ccafd92104b4a1c6eb5e7dfa94bb13d
0x002500 <tiger_pan@outlook.com>
2023-08-30 19:36:05 +0800

    [feat] The GUI for this project has been added

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,25 @@
+use screenshots::Screen;
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#[cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+#[tauri::command]
+fn getpic_core() -> bool{
+    let screens = Screen::all().unwrap();
+
+    for i in 1..5 {
+        for screen in &screens {
+            let mut image = screen.capture().unwrap();
+            image.save(format!("{}.png", i)).unwrap();
+        }
+    }
+
+    return true;
+}
 
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![getpic_core])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
