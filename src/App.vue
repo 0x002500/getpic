008bcf13f12d87ae3589aed33cb5f8312d3e357d
0x002500 <116722706+0x002500@users.noreply.github.com>
2023-09-03 13:42:10 +0800

    water

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,16 @@ import { invoke } from '@tauri-apps/api'
 <template>
   <h1>GetPic</h1>
   <input type="number" id="nos" name="number of shots" size="100" />
-  <button @click="invoke('getpic_core', { n: "5" })">Run!</button>
+  <button @click="invoke('getpic_core', { n: m })">Run!</button>
 </template>
 
-<script setup lang="js">
+<script setup lang="ts">
 function getValue() {
   // 获取输入框元素
   var inputElement = document.getElementById("nos");
   // 获取输入框的值
   var inputValue = inputElement.value;
   // 将输入框的值赋值给其他变量
-  var otherVariable = inputValue;
+  var m = inputValue;
 }
 </script>
