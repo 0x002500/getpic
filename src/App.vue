60e9255d6f8e919c6d0dd36099599f159e52a2da
0x002500 <116722706+0x002500@users.noreply.github.com>
2023-09-03 13:40:19 +0800

    Shit!

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import { invoke } from '@tauri-apps/api'
 <template>
   <h1>GetPic</h1>
   <input type="number" id="nos" name="number of shots" size="100" />
-  <button @click="invoke('getpic_core', { n: m })">Run!</button>
+  <button @click="invoke('getpic_core', { n: "5" })">Run!</button>
 </template>
 
 <script setup lang="js">
