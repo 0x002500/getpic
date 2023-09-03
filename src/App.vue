830a046dfd7785cacb390746bbba88d135f5afbb
0x002500 <116722706+0x002500@users.noreply.github.com>
2023-09-03 12:15:16 +0800

    [feat] add a js function

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,17 @@ import { invoke } from '@tauri-apps/api'
 
 <template>
   <h1>GetPic</h1>
-  <input type="number" id="number of shots" name="number of shots" size="100" />
-  <button @click = "invoke('getpic_core', {n: m})">Run!</button>
+  <input type="number" id="nos" name="number of shots" size="100" />
+  <button @click="invoke('getpic_core', { n: m })">Run!</button>
 </template>
+
+<script setup lang="js">
+function getValue() {
+  // 获取输入框元素
+  var inputElement = document.getElementById("myInput");
+  // 获取输入框的值
+  var inputValue = inputElement.value;
+  // 将输入框的值赋值给其他变量
+  var otherVariable = inputValue;
+}
+</script>
