85034324da2568dff167b6f227406ac16da6c88f
0x002500 <116722706+0x002500@users.noreply.github.com>
2023-09-05 19:47:28 +0800

    [feat] Improve the UI

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,24 @@ import { invoke } from '@tauri-apps/api'
 </script>
 
 <template>
-  <h1>GetPic</h1>
-  <input type="number" id="nos" name="number of shots" size="100" />
-  <button @click="invoke('getpic_core', { n: m })">Run!</button>
-</template>
-
-<script setup lang="ts">
-	export default {
-       name: 'Login',
-        data() {
-            return {
-                username: '',
-                password: ''
-            }
-        },
-        methods: {
-            login() {
-                   console.log(this.username)
-                   console.log(this.password)
-            }
-        }
-    }
-	<script/>
+    <h1>GetPic</h1>
+    <input type="number" name="number of shots" size="100" v-model="nos" />
+    <button @click="register()">Run!</button>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        nos: "",
+      };
+    },
+    methods: {
+      register() {
+        const inputValue = this.nos;
+        // 这里可以使用inputValue进行进一步处理或传递
+        invoke('getpic_core', { n: inputValue });
+      },
+    },
+  };
+  </script>
